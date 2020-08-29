abf43fcab727df92ed604e0af29a3b1c14246db7
hyifan9988@gmail.com <hyifan9988@gmail.com>
2020-08-28 21:50:01 -0700

    add homework 2

--- src/hw1/P8/MemoizationExample.java
+++ src/hw1/P8/MemoizationExample.java
@@ -1,4 +1,25 @@
 package hw1.P8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoizationExample {
+    public Map<Integer,Integer> memorizeTable = new HashMap<>();
+    //Fibonacci with Memoization;
+    public int fibonacci(int n) {
+        if (n == 0) { return 0;}
+        if (n == 1) { return 1;}
+        if (this.memorizeTable.containsKey(n)){
+            return this.memorizeTable.get(n);
+        }
+        int result = fibonacci(n-1) + fibonacci(n-2);
+        this.memorizeTable.put(n,result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MemoizationExample memoizationExample = new MemoizationExample();
+        int testRes = memoizationExample.fibonacci(5);
+        System.out.println(testRes);
+    }
 }
